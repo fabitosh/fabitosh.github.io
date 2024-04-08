05e6e9546b3004e57a3bf4c6a1c34127e0c5f3a5
Fabio Meier <fabitosh@gmail.com>
2024-04-08 07:14:54 +0200

    update references

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,13 @@
 const fg = require("fast-glob");
 
-photographyImages = fg.sync(['photography/**/*.jpg', '!**/_site']);
+photographyImages = fg.sync(['pages/photography/**/*.jpg', '!**/_site']);
 
 module.exports = function(eleventyConfig) {
     eleventyConfig.addCollection('notes', function(collectionApi) {
-        return collectionApi.getFilteredByGlob('notes/*.md');
+        return collectionApi.getFilteredByGlob('pages/notes/*.md');
     });
 
-    eleventyConfig.addPassthroughCopy("photography/800px/");
+    eleventyConfig.addPassthroughCopy("pages/photography/800px/");
     eleventyConfig.addCollection('photographyJpgs', function(collection) {
         return photographyImages});
 };
